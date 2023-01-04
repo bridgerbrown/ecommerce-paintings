bf2ab439b6c74b723782cea3c1e222097190607c
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-01-04 14:09:54 -0800

    fb config

--- components/firebase/firebase.config.js
+++ components/firebase/firebase.config.js
@@ -1,15 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";  
 import { getAuth } from "firebase/auth"
+import { firebaseConfig } from "./firebase-config-keys";
 
-export const firebaseConfig = {
-    apiKey: "AIzaSyD-93dTvcqceIgA7eiJ-ZTGGuP0pgSfR8I",
-    authDomain: "ecommerce-f2425.firebaseapp.com",
-    projectId: "ecommerce-f2425",
-    storageBucket: "ecommerce-f2425.appspot.com",
-    messagingSenderId: "342964477004",
-    appId: "1:342964477004:web:0c45cbd1a73981775b68e9"
-}
+
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
