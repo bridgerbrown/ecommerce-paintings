276c76eecd572f46652d32193b34cc22dd5c4480
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-01-04 15:42:25 -0800

    footer url fix

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="www.bridgerbrown.dev" target="_blank" rel="noopener noreferrer">Created by Bridger Brown</a>
+            <a href="http://www.bridgerbrown.dev" target="_blank" rel="noopener noreferrer">Created by Bridger Brown</a>
         </div>
     )
 }
