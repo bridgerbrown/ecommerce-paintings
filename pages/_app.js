0728125178eb9b0fb4ac6eb741d70f345f776e37
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-10-31 16:42:57 -0700

    babel runtime fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import React from "react";
 import { AuthUserContextProvider } from '../data/context/AuthUserContext';
 import { ProductProvider } from '../data/context/ProductContext';
 
